6f14a092f0099904274ef7d5080c88be1049cde9
susharan1997 <37873564+susharan1997@users.noreply.github.com>
2024-07-14 14:18:06 +0100

    Create App.styles.ts

---
+++ src/App.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import IconButton from "@mui/material/IconButton";
+
+export const Wrapper = styled.div``;
+
+export const StyledButton = styled(IconButton)`
+  position: fixed;
+  z-index: 100;
+  top: 40px;
+`;
