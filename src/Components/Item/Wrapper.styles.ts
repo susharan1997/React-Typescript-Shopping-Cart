cae26cc0049ffd5d217292dcda80b1af6ae79e8f
susharan1997 <37873564+susharan1997@users.noreply.github.com>
2024-07-14 14:23:59 +0100

    Add files via upload

---
+++ src/Components/Item/Wrapper.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  flex-direction: column;
+  border: 1px solid black;
+  border-radius: 30px;
+`;
